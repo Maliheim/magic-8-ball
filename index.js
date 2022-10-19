8f74841cce28089604818c799042eb6d46a59745
Maliheim <reeveslance@gmail.com>
2022-10-18 21:11:41 -0700

    Created random number selector function || Created function to use random number selector to display img associated with that number || Added event listener that triggers previously stated functions

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-let answerimgs = [];
+let inputField = document.getElementById("questionInput");
+let inputBtn = document.getElementById("inputBtn");
 
-let newImg = (src) => {
-    
-  src = `./images/magic8ball_${1}.png`;
-
-  document.answers.appendChild();
+let randomNum = () => {
+  let min = 1;
+  let max = 21;
+  return Math.floor(Math.random() * (max - min) + min);
 };
+console.log(randomNum());
+
+let newImg = () => {
+  let img = `./images/magic8ball_${randomNum()}.png`;
+  let answerImg = document.createElement("img");
+
+  answerImg.src = img;
+  answerImg.alt = `magic 8 ball img`;
+
+  document.querySelector(".answers").appendChild(answerImg);
+};
+
+inputBtn.addEventListener("click", () => {
+  if (inputField.value) {
+    newImg();
+  } else {
+    console.log("No Question");
+  }
+});
+
+//// make img generated draw from random number that is generated
+// TODO To do Highlight
+//   Normal Comment
+// * Highlighted Comment
+// ? Blue Comment
+// ! Red Comment
