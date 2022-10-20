d4603a000182eb15b818b52fe35af125b9c9d72d
Maliheim <reeveslance@gmail.com>
2022-10-19 21:07:13 -0700

    Made animation in JS that lets new 8ball image fade in over time once ask question button is clicked || Added a New Question button that still needs to be scripted

--- index.js
+++ index.js
@@ -11,11 +11,20 @@ console.log(randomNum());
 let newImg = () => {
   let img = `./images/magic8ball_${randomNum()}.png`;
   let answerImg = document.createElement("img");
+  answerImg.className = "answerImg";
 
   answerImg.src = img;
   answerImg.alt = `magic 8 ball img`;
+  answerImg.style.opacity = 0;
+  answerImg.style.transition = "opacity 3s ease";
 
   document.querySelector(".answers").appendChild(answerImg);
+  setTimeout(() => {
+    document.querySelector(".answerImg").style.opacity = 1;
+  }, 1);
+  //   Because changes to the document are batched and performed all at the same time we need to use a
+  //   setTimeout here so the opacity doesnt change to 1 till after the image has been written to the document.
+  //   Not at the same time.
 };
 
 inputBtn.addEventListener("click", () => {
