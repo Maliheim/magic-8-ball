a8f77b823c1ac95f90d311a73afcfb379d2f5f54
Maliheim <reeveslance@gmail.com>
2022-10-24 20:03:22 -0700

    finished js animations || 8 ball complete

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ let inputField = document.getElementById("questionInput");
 let inputBtn = document.getElementById("inputBtn");
 let newBtn = document.getElementById("newBtn");
 let baseImg = document.getElementById("baseImg");
+let questionField = document.querySelector(".questionField");
 
 let randomNum = () => {
   let min = 1;
@@ -9,28 +10,33 @@ let randomNum = () => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-let shakeImg = () => {};
-
 let newImg = () => {
   let img = `./images/magic8ball_${randomNum()}.png`;
-  let answerImg = document.createElement("img");
-  answerImg.className = "answerImg";
+  //   let answerImg = document.createElement("img");
+  //   answerImg.className = "answerImg";
+  document.querySelector("#baseImg").style.animation =
+    "shaking 4s ease 0s 1, jumping 4s ease 0s 1";
+
+  let answerImg = document.querySelector(".answerImg");
 
   answerImg.src = img;
   answerImg.alt = `magic 8 ball img`;
   answerImg.style.opacity = 0;
-  answerImg.style.transition = "opacity 3s ease";
+  answerImg.style.transition = "opacity 4s ease";
 
   document.querySelector(".answers").appendChild(answerImg);
   setTimeout(() => {
     document.querySelector(".answerImg").style.opacity = 1;
-  }, 1);
+    document.querySelector("#baseImg").style.animation = "none";
+  }, 4500);
+
   //   Because changes to the document are batched and performed all at the same time we need to use a
   //   setTimeout here so the opacity doesnt change to 1 till after the image has been written to the document.
   //   Not at the same time.
 };
 
-inputBtn.addEventListener("click", () => {
+questionField.addEventListener("submit", (e) => {
+  e.preventDefault();
   if (inputField.value) {
     newImg();
   } else {
@@ -40,8 +46,10 @@ inputBtn.addEventListener("click", () => {
 
 newBtn.addEventListener("click", () => {
   inputField.value = "";
-  document.querySelector(".answers").innerHTML = "";
+  document.querySelector(".answerImg").style.transition = "opacity 0.5s ease";
+  document.querySelector(".answerImg").style.opacity = 0;
 });
+
 // TODO || Make default img reappear after clearing inner HTML ||
 //// make img generated draw from random number that is generated
 // TODO To do Highlight
